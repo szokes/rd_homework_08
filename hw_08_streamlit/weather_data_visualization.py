4eea7a6489b98d5d89a002db69da13d0e4b71ca0
szokes <szoke@helion.hu>
2026-05-29 00:51:14 +0200

    Update weather_data_visualization.py

---
+++ hw_08_streamlit/weather_data_visualization.py
@@ -0,0 +1,11 @@
+import requests
+import pandas as pd
+import streamlit as st
+
+def main():
+    st.title("Robot Dreams Python - Weather Map & Data Visualization App")
+    st.subheader("(by Sándor Szőke)")
+
+
+if __name__ == "__main__":
+    main()
